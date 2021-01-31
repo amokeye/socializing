5d526d471e176a4d739164838daccd8d668cd09e
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-30 18:08:02 -0800

    updated user model

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: 'Username required.'
+        },
+        email: {
+            type: String,
+            required: 'Email address required.',
+            unique: true,
+            match: [/.+\@.+\..+/, 'Email address required.']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
